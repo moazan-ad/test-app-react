2b0d1f4b53123e0321ccf952d1f2281fd541d661
Sannan <fazal@agiledisrict.com>
2024-11-25 14:48:32 +0500

    feat: complete the user table

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import UserForm from './components/user-form/UserForm'
-
+import UserTable from './components/user-table/UserTable'; 
 
 function App() {
   
-  
-
   const [userList, setUserList] = useState([]);
+  const [toggleComponents, setToggleComponents] = useState('user-form')
 
+  const handleToggleComponents = (componentName) => {
+    setToggleComponents(componentName);
+  }
 
-  const addToUserList = (user) => {
+  const addToUserList = ({user}) => {
     setUserList(prev => [...prev, user])
   }
 
-
   return (
     <main>
-      <UserForm addToUserList={addToUserList}/>
+      
+      {
+        toggleComponents === "user-form" && <UserForm handleToggleComponents={handleToggleComponents}
+          addToUserList={addToUserList} />
+      }
+      {
+        toggleComponents === "user-table" && <UserTable handleToggleComponents={handleToggleComponents}
+          userList={userList} />
+      }
+
     </main>
   )
 }
